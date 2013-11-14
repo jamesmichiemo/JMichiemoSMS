dfb9239019d1ab3c976d96e0372b9b0502cc8ab7
James Michiemo <james.michiemo@gmail.com>
2013-11-13 21:00:29 -0500

    update player

--- js/application.js
+++ js/application.js
@@ -1,33 +1,84 @@
 var flashReady = function() {
-played();
-function played(){
-
-  $('.play').click(function(){
-    var par = $(this).parent();
-    flash.connect('rtmp://localhost/SMSServerTwo');
-    $(this).remove();
-    $(par).prepend("<li><img class='stop' src='img/stop.png' alt='stop' height='20' width='20'></li>");
-    paused();
-  });
-}
-function paused(){
-  $('.stop').click(function(){
-    var par = $(this).parent();
-    flash.playPause();
-    $(this).remove();
-    $(par).prepend("<img class='play' src='img/play.png' alt='play' height='20' width='20'>");
-    played();
-  });
-}
+  flash.connect('rtmp://localhost/SMSServerTwo');
 };
 
+var playing = false;
+$('.play').click(function(e){
+  if(!playing){
+    flash.playPause();
+    $('.play img').attr('src', 'img/play.png');
+    playing = true;
+  } else{
+    flash.playPause();
+    $('.play img').attr('src', 'img/stop.png');
+    playing = false;
+  }
+  e.preventDefault();
+  return false;
+});
 
-
+var recording = false;
+$('.rec').on('click', function(e){
+  if(!recording){
+    flash.startRecording('movie', 0, 0);
+    recording = true;
+  } else {
+    flash.stopRecording();
+    flash.startPlaying('hobbit_vp6.flv');
+    recording = false;
+  }
+  e.preventDefault();
+  return false;
+});
+  
 var connected = function(success,error){
   if(success){
     flash.startPlaying("hobbit_vp6.flv");
+    $('.play img').attr('src', 'img/stop.png');
   } 
 };
+
+var videoDuration;
+var totalDuration;
+
+
+var getDuration = function(time){
+  videoDuration = time;
+  var videoDurationMin = Math.floor(time/60);
+  var videoDurationSec = Math.floor(time % 60);
+  $('#time').text(totalDuration = videoDurationMin + ':' + videoDurationSec);
+  console.log('get duration');
+  
+}
+
+var seekTime = function(time){
+  var currentMin = Math.floor(time/60);
+  var currentSec = Math.floor(time % 60);
+
+  if (currentSec < 10){
+    $('#time').text(currentMin + ':0' + currentSec + " / " + totalDuration); 
+  } else{
+    $('#time').text(currentMin + ':' + currentSec + ' / ' + totalDuration);
+  }  
+  
+}
+
+$('.seekbar').on('click', function(e){
+  var left = e.pageX - $(this).offset().left;
+  var perc = left / $('.seekbar').width();
+  var time = perc * videoDuration;
+
+  $('.seek_cursor').css('margin-left', left+'px');
+
+  flash.setTime(time);
+
+  e.preventDefault();
+  return false;
+
+
+});
+
+  
 
 
 
